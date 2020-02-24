89c8ab3281b2eeefc626aaac2ac5ee9bbb422492
Brad Flaugher <brad@bradflaugher.com>
2020-02-24 13:04:39 -0500

    tailing log

--- mysite/cmdexample/views.py
+++ mysite/cmdexample/views.py
@@ -14,3 +14,18 @@ def index(request):
     return HttpResponse(template.render(context,request))
 
     
+def tail_log(request):
+    #TODO put some max size here or keep track of the line you're reading from or something?
+    
+    log_output = ""
+    with open(os.path.join(os.getcwd(), 'my_log.txt'),'rU') as f:
+        for line in f:
+            log_output += line
+
+    template = loader.get_template('templates/log.html')
+    context = {
+        'log_output': log_output
+    }
+    return HttpResponse(template.render(context,request))
+
+    
